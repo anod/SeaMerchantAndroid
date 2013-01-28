1b5a2b96d971f29d4cabf67bcb74cdbd98e3ac85
Eran Zimbler <eran@zimbler.net>
2013-01-28 18:35:39 +0200

    reordered location numbers

--- SeaMerchantApp/src/com/example/seamerchant/game/Location.java
+++ SeaMerchantApp/src/com/example/seamerchant/game/Location.java
@@ -3,9 +3,9 @@ package com.example.seamerchant.game;
 
 public class Location {
 	
-	public static final int ISRAEL = 1;
-	public static final int EGYPT = 2;
-	public static final int TURKEY = 3;
+	public static final int EGYPT = 3;
+	public static final int ISRAEL = 2;
+	public static final int TURKEY = 1;
 	
 	private int mType;
 	private PricedItem wheat;
